c0cc2317509482a9339cf0cd457e0546cd7eb405
farhan0511 <programingsukses@gmail.com>
2025-11-02 12:05:53 +0700

    menambahkan routing untuk login register

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import ErorPage from "./components/Layouts/404.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World!</div>,
+    errorElement: <ErorPage/>
+  },
+  {
+    path: "/Login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/Register",
+    element: <RegisterPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
